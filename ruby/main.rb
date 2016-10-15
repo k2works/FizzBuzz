d189faf711a185bb36f1a7170c4f4ab41c5254d2
k2works <kakimomokuri@gmail.com>
2016-10-15 13:03:09 +0900

    Complete First TestCase

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,5 +1,23 @@
 class FizzBuzz
   def execute
-    [*(1..100)]
+    arr = []
+    (1..100).each do |n|
+      if n%3 == 0
+        if n%5 == 0
+          arr << 'FizzBuzz'
+        else
+          arr << 'Fizz'
+        end
+      elsif n%5 == 0
+        if n%3 == 0
+          arr << 'FizzBuzz'
+        else
+          arr << 'Buzz'
+        end
+      else
+        arr << n
+      end
+    end
+    arr
   end
 end
