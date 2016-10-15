b92585e114e084b852c7c9a8acd045b4558907a5
k2works <kakimomokuri@gmail.com>
2016-10-15 12:19:06 +0900

    Complete First TestCase

--- javascript/app/scripts/main.js
+++ javascript/app/scripts/main.js
@@ -1,9 +1,22 @@
 var fizzbuzz = function () {
   var arr = [];
   for(var i=1; i<=100; i++) {
-    arr.push(i);
+    if((i%3) === 0){
+      if((i%5) === 0){
+        arr.push('FizzBuzz');
+      } else {
+        arr.push('Fizz');
+      }
+    } else if((i%5) === 0){
+      if((i%3) === 0){
+        arr.push('FizzBuzz');
+      } else {
+        arr.push('Buzz');
+      }
+    } else {
+      arr.push(i);
+    }
   }
-
   return arr;
 };
 
